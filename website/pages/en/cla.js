877e8643f74e8c69a0d0a5b26af17fb160eac13b
Florian Rohrer <florian.rohrer@vivaldi.net>
2019-02-11 15:06:14 +0100

    removed doc and added CLA Page

---
+++ website/pages/en/cla.js
@@ -0,0 +1,32 @@
+const React = require('react');
+
+const CompLibrary = require('../../core/CompLibrary.js');
+
+const Container = CompLibrary.Container;
+
+class Users extends React.Component {
+  render() {
+    
+    return (
+      <div className="mainContainer">
+        <Container padding={['bottom', 'top']}>
+          <div className="showcaseSection">
+            <div className="prose">
+              <h1>Contributor License Agreement</h1>
+              <p>This is our Contributor License Agreement</p>
+            </div>
+            <div>
+                FOO
+                $vsjfkldsa
+                fds
+                afdsa
+                
+            </div>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+}
+
+module.exports = Users;
