78a07daadc465e671d1c216d30d745775e25fe7e
minhsl <khanh.nguyen@swisslife.ch>
2019-05-20 10:46:30 +0200

    Added highlight theme tomorrow nights

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -67,7 +67,7 @@ const siteConfig = {
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
+    theme: 'tomorrow-night',
   },
 
   // Add custom scripts here that would be placed in <script> tags.
